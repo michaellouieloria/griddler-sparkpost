cd617f698eda1ee221e3fb3e087d5ed1e3354c3d
Michael Louie Loria <michaellouieloria@gmail.com>
2017-02-10 17:12:15 +0800

    fix bug

--- lib/griddler/sparkpost/adapter.rb
+++ lib/griddler/sparkpost/adapter.rb
@@ -13,7 +13,7 @@ module Griddler
       end
 
       def normalize_params
-        beging
+        begin
           msg = params['_json'][0]['msys']['relay_message']
           content = msg['content']
           mail = Mail.read_from_string(content['email_rfc822'])
